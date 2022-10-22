cae7c3ca8349c4a9db840122375215d54f191d08
serinko <johanesxsalmon@riseup.net>
2022-10-22 09:13:05 +0200

    sort string list to array

--- numpy_and_vectorized_computation/custom_fns.py
+++ numpy_and_vectorized_computation/custom_fns.py
@@ -1,1 +1,12 @@
 """A template of useful functions"""
+
+# Don't forget to import modules needed for different functions.
+import numpy as np
+
+def create_sorted_string_arr(lst,collumns=3):
+    """Convert a list of strings into a sorted array with desired n of colummns."""
+    lst = sorted(lst)
+    [lst.append("") for i in range(collumns - len(lst)%collumns)]
+    rows = int(len(lst)/collumns)
+    arr = np.array(lst).reshape(rows,collumns)
+    return arr

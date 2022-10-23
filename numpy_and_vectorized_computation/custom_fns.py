062bf7811311c0882d5aa738521f5d595abbba20
serinko <johanesxsalmon@riseup.net>
2022-10-23 09:02:44 +0200

    get_sorted_str_array()

--- numpy_and_vectorized_computation/custom_fns.py
+++ numpy_and_vectorized_computation/custom_fns.py
@@ -3,10 +3,11 @@
 # Don't forget to import modules needed for different functions.
 import numpy as np
 
-def create_sorted_string_arr(lst,collumns=3):
+def get_sorted_str_array(lst,collumns=3):
     """Convert a list of strings into a sorted array with desired n of colummns."""
     lst = sorted(lst)
-    [lst.append("") for i in range(collumns - len(lst)%collumns)]
+    x = columns - len(lst)%columns
+    [lst.append("") for i in range(x) if x < columns]
     rows = int(len(lst)/collumns)
     arr = np.array(lst).reshape(rows,collumns)
     return arr

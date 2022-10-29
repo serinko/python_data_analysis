af437e1b91b0e56615b8eb124d2be2c93ed36c30
serinko <johanesxsalmon@riseup.net>
2022-10-29 08:59:09 +0200

    starting with pandas

--- python_tools/pickle_load_dump.py
+++ python_tools/pickle_load_dump.py
@@ -1,9 +1,16 @@
 """Pickle - basic import and export"""
 
 import pickle 
+#with open("data.pckl", "w") as f:
+#    pickle.dump(data,f)
 
-with open("data.pckl", "w") as f:
-    pickle.dump(data,f)
+objects = []
 
-with open("data.pckl") as f:
-    data = pickle.load(f)
+with open("~/src/narodnik/coins-price-jan-mar-22.pkl", "rb") as f:
+#    data = pickle.load(f)
+    while True:
+        try:
+            objects.append(pickle.load(f))
+        except EOFError:
+            break
+print(objects)
